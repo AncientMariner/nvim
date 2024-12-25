f9b01bf39b12bf075abdbda6414cac3a12a302fb
AncientMariner <xander.lysak@gmail.com>
2024-12-25 13:20:23 +0100

    add telescope search in hidden files

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -16,8 +16,19 @@ return {
         local actions = require("telescope.actions")
         local lga_actions = require("telescope-live-grep-args.actions")
 
+		local telescopeConfig = require("telescope.config")
+		-- Clone the default Telescope configuration
+		local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+		-- I want to search in hidden/dot files.
+		table.insert(vimgrep_arguments, "--hidden")
+		-- I don't want to search in the `.git` directory.
+		table.insert(vimgrep_arguments, "--glob")
+		table.insert(vimgrep_arguments, "!**/.git/*")
+
         telescope.setup({
             defaults = {
+				-- `hidden = true` is not supported in text grep commands.
+				vimgrep_arguments = vimgrep_arguments,
                 sorting_strategy = "ascending",
                 layout_strategy = "horizontal",
                 layout_config = {prompt_position = "top"},
@@ -33,6 +44,12 @@ return {
                     }
                 }
             },
+			pickers = {
+				find_files = {
+					-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+				},
+			},
             extensions = {
                 file_browser = {
                     path = "%:p:h", -- open from within the folder of your current buffer
